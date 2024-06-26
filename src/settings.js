12010627afb318fec56276f0e5cf9f5af6b1ce12
zl <weixin_67448099@noreply.gitcode.com~>
2024-06-26 21:44:44 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+// 网站的标题
+  title: '管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description 头部导航是否进行固定定位
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description 是否显示导航栏上方的logo
+   */
+  sidebarLogo: true
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Date: 2024-06-26 16:07:46
+ * @LastEditors: zl weixin_67448099@noreply.gitcode.com~
+ * @LastEditTime: 2024-06-26 18:46:01
+ * @FilePath: \vue-admin-template\src\views\dashboard\index.vue
+-->
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- 使用svg图标 -->
+    <svg-icon icon-class="example" />
+    <svg-icon icon-class="drag" />
+    <svg-icon icon-class="drag" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters(['name'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
