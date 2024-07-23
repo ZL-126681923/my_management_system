6303e5bca2e5b32eb1d5bfb754c6112bee2325f6
张良 <1077167261@qq.com>
2024-07-23 20:38:13 +0800

    实现了添加用户功能

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,18 +1,17 @@
 <!--
  * @Date: 2024-07-10 13:46:26
  * @LastEditors: 张良 1077167261@qq.com
- * @LastEditTime: 2024-07-23 15:10:41
+ * @LastEditTime: 2024-07-23 20:09:14
  * @FilePath: \My-admin\src\views\role\index.vue
 -->
 <template>
   <div class="container">
     <div class="app-container">
       <div class="role-operate">
-        <el-button size="mini" type="primary" class="btn">新增角色</el-button>
+        <el-button size="mini" type="primary" class="btn" @click="showDialog=true">新增角色</el-button>
       </div>
       <!-- 放置table组件 -->
-      <el-table :data="list" border
-      highlight-current-row>
+      <el-table :data="list" border highlight-current-row>
         <!-- 放置列 -->
         <el-table-column prop="name" align="center" width="200" label="角色" />
         <el-table-column prop="state" align="center" width="200" label="启用">
@@ -38,9 +37,43 @@
       <!-- 放置分页组件 -->
       <el-row type="flex" style="height: 60px" align="middle" justify="end">
         <!-- 放置分页组件 -->
-        <el-pagination layout="prev, pager, next" />
+        <el-pagination
+          :page-size="pageParams.pagesize"
+          :current-page="pageParams.page"
+          :total="pageParams.total"
+          layout="prev, pager, next"
+          @current-change="changePage"
+        />
       </el-row>
     </div>
+    <!-- 添加用户弹层 -->
+    <el-dialog width="500px" title="新增角色" :visible.sync="showDialog">
+      <!-- 表单内容 -->
+      <el-form label-width="120px">
+        <el-form-item label="角色名称">
+          <el-input style="width: 300px" size="mini" />
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch size="mini" />
+        </el-form-item>
+        <el-form-item label="角色描述">
+          <el-input
+            type="textarea"
+            :rows="3"
+            style="width: 300px"
+            size="mini"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-row type="flex" justify="center">
+            <el-col :span="12">
+              <el-button type="primary" size="mini">确定</el-button>
+              <el-button size="mini">取消</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -50,6 +83,12 @@ export default {
   data() {
     return {
       list: [],
+      showDialog:false,
+      pageParams: {
+        page: 1, // 第几页
+        pagesize: 5, // 每页多少条
+        total: 0, //请求条数
+      },
     };
   },
   created() {
@@ -57,8 +96,14 @@ export default {
   },
   methods: {
     async getRoleList() {
-      const { rows } = await getRoleList();
+      const { rows, total } = await getRoleList(this.pageParams);
       this.list = rows; //拿到数据
+      this.pageParams.total = total;
+    },
+    // 切换分页时 请求新的数据
+    changePage(newPage) {
+      this.pageParams.page = newPage; // 赋值当前页码
+      this.getRoleList();
     },
   },
 };
