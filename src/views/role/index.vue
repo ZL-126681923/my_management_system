4680bb4b0408436a48e0889d425e7fc517a3f2f9
张良 <1077167261@qq.com>
2024-07-23 14:26:30 +0800

    完成角色管理的页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,12 +1,38 @@
+<!--
+ * @Date: 2024-07-10 13:46:26
+ * @LastEditors: 张良 1077167261@qq.com
+ * @LastEditTime: 2024-07-23 14:25:04
+ * @FilePath: \My-admin\src\views\role\index.vue
+-->
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <div class="role-operate">
+        <el-button size="mini" type="primary">添加角色</el-button>
+      </div>
+      <!-- 放置table组件 -->
+      <el-table>
+        <!-- 放置列 -->
+        <el-table-column align="center" width="200" label="角色" />
+        <el-table-column align="center" width="200" label="启用" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作" />
+      </el-table>
+      <!-- 放置分页组件 -->
+      <el-row type="flex" style="height:60px" align="middle" justify="end">
+        <!-- 放置分页组件 -->
+        <el-pagination layout="prev, pager, next" />
+      </el-row>
     </div>
-  </div>
+    </div> 
 </template>
 <script>
 export default {
-  name: 'Role'
-}
+  name: "Role",
+};
 </script>
+<style scoped>
+.role-operate {
+  padding: 10px;
+}
+</style>
