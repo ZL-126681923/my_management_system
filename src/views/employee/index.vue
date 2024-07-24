670a7d79c4677d6b2e2956c08f819177feb6a0be
张良 <1077167261@qq.com>
2024-07-25 03:41:59 +0800

    完成左侧的树形结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,12 +1,95 @@
+<!--
+ * @Date: 2024-07-10 13:46:26
+ * @LastEditors: 张良 1077167261@qq.com
+ * @LastEditTime: 2024-07-25 03:38:48
+ * @FilePath: \My-admin\src\views\employee\index.vue
+-->
 <template>
   <div class="container">
     <div class="app-container">
-      员工
+      <div class="left">
+        <el-input
+          style="margin-bottom: 10px"
+          type="text"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="输入员工姓名全员搜索"
+        />
+        <!-- 树形组件 -->
+        <el-tree
+          :data="depts"
+          :props="defaultProps"
+          default-expand-all
+          :expand-on-click-node="false"
+          highlight-current
+  />
+      </div>
+      <div class="right">
+        <el-row class="opeate-tools" type="flex" justify="end">
+          <el-button size="mini" type="primary">添加员工</el-button>
+          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini">excel导出</el-button>
+        </el-row>
+        <!-- 表格组件 -->
+        <!-- 分页 -->
+      </div>
     </div>
   </div>
 </template>
+
 <script>
+import{getDepartment}from '@/api/department'
+import{transListToTreeData}from '@/utils'
 export default {
-  name: 'Employee'
-}
+  name: "Employee",
+  data() {
+    return {
+      depts: [], // 组织数据
+      defaultProps: {
+        label: "name",
+        children: "children",
+      },
+    };
+  },
+  created() {
+    this.getDepartment()
+  },
+  methods: {
+    async getDepartment() {
+      // 递归方法 将列表转化成树形
+      // let result = await getDepartment()
+      this.depts = transListToTreeData(await getDepartment(), 0)
+    }
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.app-container {
+  background: #fff;
+  display: flex;
+  .left {
+    width: 280px;
+    padding: 20px;
+    border-right: 1px solid #eaeef4;
+  }
+  .right {
+    flex: 1;
+    padding: 20px;
+    .opeate-tools {
+      margin: 10px;
+    }
+    .username {
+      height: 30px;
+      width: 30px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 50%;
+      color: #fff;
+      background: #04c9be;
+      font-size: 12px;
+      display: inline-block;
+    }
+  }
+}
+</style>
